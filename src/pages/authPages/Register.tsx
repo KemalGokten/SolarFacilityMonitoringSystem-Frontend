293acfe75ab2f469dc926f41e6901b42dc00bf8d
KemalGokten <ukemalgokten@gmail.com>
2024-08-03 20:02:59 +0200

    Implement registration page with form validation and GraphQL mutation in Register.tsx

---
+++ src/pages/authPages/Register.tsx
@@ -0,0 +1,158 @@
+import { useRegisterUserMutation } from "../../graphql/generated";
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useLoginUserMutation } from "../../graphql/generated";
+import { useNavigate } from "react-router-dom";
+
+// Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+const passwordValidation = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/
+);
+
+// Form schema with username
+const schema = z.object({
+  username: z.string().min(1, { message: "Username is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().regex(passwordValidation, {
+    message:
+      "Password must contain at least 6 character(s), one uppercase letter, one lowercase letter, one number and one special character",
+  }),
+});
+
+type SignupForm = z.infer<typeof schema>;
+
+// Default theme
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+  const [registerUserMutation, { data, loading, error }] =
+    useRegisterUserMutation();
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupForm>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  // Submitting the form
+  const submitSignupForm: SubmitHandler<SignupForm> = async (formData) => {
+    try {
+      const response = await registerUserMutation({ variables: formData });
+      if (response.data) {
+        navigate("/auth/login"); // Redirect to home page or another page after registration
+      }
+    } catch (e) {
+      console.error("Registration error", e);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit(submitSignupForm)}
+            noValidate
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="Name"
+                  required
+                  fullWidth
+                  id="Name"
+                  label="Name"
+                  autoFocus
+                  error={!!errors.username}
+                  helperText={errors.username?.message}
+                  {...register("username")}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  autoComplete="email"
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                  {...register("email")}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
+                  {...register("password")}
+                />
+              </Grid>
+            </Grid>
+            {error && <Typography color="error">{error.message}</Typography>}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={isSubmitting || loading}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/auth/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
