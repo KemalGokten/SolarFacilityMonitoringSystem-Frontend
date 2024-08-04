5a4072ed9020ea3ae115662c6fb227bd6ad8a5cc
KemalGokten <ukemalgokten@gmail.com>
2024-08-04 18:15:40 +0200

    Change monitoring icon

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -4,7 +4,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LegendToggleIcon from '@mui/icons-material/LegendToggle';
 
 export const MainListItems = () => {
   const location = useLocation();
@@ -27,7 +27,7 @@ export const MainListItems = () => {
         selected={location.pathname === "/monitoring"}
       >
         <ListItemIcon>
-          <ShoppingCartIcon />
+          <LegendToggleIcon />
         </ListItemIcon>
         <ListItemText primary="Monitoring" />
       </ListItemButton>
